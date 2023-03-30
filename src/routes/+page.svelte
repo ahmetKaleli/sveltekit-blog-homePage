83a744c34c840337be82d267ea6ee0c6b3b96d8e
ahmo59 <ahmetkaleli61@gmail.com>
2023-03-30 12:31:18 +0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,72 @@
+
+    <div class="header">
+        <div style="position:fixed">
+            <h1><a href="/">KELLY</a></h1>
+            <ul>
+               <a href="home/"><li>Home</li></a>
+               <a href="/"><li>Branding</li></a>
+               <a href="*"><li>Design</li></a>
+               <a href="*"><li>Identity</li></a>
+               <a href="*"><li>Furniture</li></a>
+               <a href="*"><li>Blog</li></a>
+               <a href="*"><li>Contact</li></a>
+            </ul>
+        </div>
+   
+    </div>
+    <div class="içerik">
+        <img src="src/img/at3.jpg" alt=""/>
+        <img src="src/img/at4.jpg" alt=""/>
+        <img src="src/img/drevan.jpg" alt=""/>
+        <img src="src/img/dvader.jpg" alt=""/>
+        <img src="src/img/luke.jpg" alt=""/>
+        <img src="src/img/mandolarıan.jpg" alt=""/>
+    </div> 
+    
+<style>
+
+
+     .header{
+        position: relative;
+        width: 450px;
+	    margin-top:15px;
+        margin-left: 190px;
+        float:left;	
+    }
+
+
+    .header h1{
+        text-align: center;
+    }
+    .header a {
+        text-decoration: none;
+        list-style-type: none;
+        color:black;
+    }
+    .header ul {
+        list-style-type:none;
+    }
+
+    .header ul li{
+        text-align: center;
+        margin-top: 12px;
+        color:gray;
+    }
+
+    .header ul li:hover{
+        color:black;
+    }
+    .içerik{
+	    width:550px;
+	    height:350px;
+	    float:right;
+	    margin-top:15px;
+        margin-right: 250px;
+
+    }
+    .içerik img{
+        width: 550px;
+        height: 400px;
+    }
+
+</style>
